2b60fc1d883dc0b5a6c8385ed9d65a2785bd341b
Ivan Arias <arias.ivan@gmail.com>
2024-05-27 23:27:34 -0400

    app updated

--- src/app.py
+++ src/app.py
@@ -1,31 +1,27 @@
-# The following environment is selected: ~/dash-google/.venv/bin/python
 import pandas as pd
 from dash import Dash, dcc, html, Input, Output, dash_table
 import dash_bootstrap_components as dbc
 import plotly.express as px
 
-# df = pd.read_csv("grades_over_time.csv")
+# Load the new dataset
 df = pd.read_csv('https://raw.githubusercontent.com/hcoco1/Dashboard-Plothy-Dash/main/hcoco1/src/data/grades_over_time.csv')
 
-
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
 server = app.server
-
 
 # Ensure grades are numeric
 for col in df.columns[3:]:
     df[col] = pd.to_numeric(df[col], errors="coerce")
 
 # Calculate final grades by averaging sublevels
-subjects = ["Math", "Science", "English", "History", "Geography", "Art"]
+subjects = ["Matematicas", "Literatura", "English", "Deporte", "Geography", "Art", "Biologia", "Orientacion", "Participacion"]
 for subject in subjects:
     df[f"{subject}"] = df[
         [f"{subject} Exam 1", f"{subject} Exam 2", f"{subject} Exam 3"]
-    ].mean(axis=1).round(2)  # Round to 2 decimal places
+    ].mean(axis=1).round(2)
 
 # Calculate the final grade average across all subjects
-df["Grade Average"] = df[[f"{subject}" for subject in subjects]].mean(axis=1).round(2)  # Round to 2 decimal places
+df["Grade Average"] = df[[f"{subject}" for subject in subjects]].mean(axis=1).round(2)
 
 # Prepare the summary table with only final grades
 summary_df = df[
@@ -37,14 +33,13 @@ summary_df = df[
 # Ensure all final grades have no more than two decimal places
 summary_df = summary_df.round(2)
 
-
 # Layout of the app
 app.layout = dbc.Container(
     [
         dbc.Row(
             [
                 dbc.Col(
-                    html.H1("High School Students' Grades Dashboard"), className="mb-4"
+                    html.H1("Dashboard Interactivo Rendimiento Estudiantil"), className="mb-4"
                 )
             ]
         ),
@@ -64,7 +59,7 @@ app.layout = dbc.Container(
                         ),
                     ],
                     width=4,
-                    style={"margin-bottom": "0px"},  # Custom margin
+                    style={"margin-bottom": "0px"},
                 ),
                 dbc.Col(
                     [
@@ -111,7 +106,7 @@ app.layout = dbc.Container(
                     style={"marginLeft": "auto", "border": "none"},
                 ),
             ],
-            className="align-items-start",  # Ensure top alignment
+            className="align-items-start",
         ),
         dbc.Row(
             [
@@ -129,7 +124,7 @@ app.layout = dbc.Container(
                         ),
                     ],
                     width=4,
-                    style={"margin-top": "0px"},  # Custom margin
+                    style={"margin-top": "0px"},
                 )
             ]
         ),
@@ -147,12 +142,15 @@ app.layout = dbc.Container(
                         columns=[
                             {"name": "Student Name", "id": "Name"},
                             {"name": "Año Escolar", "id": "Year"},
-                            {"name": "Matematica", "id": "Math"},
-                            {"name": "Ciencia", "id": "Science"},
-                            {"name": "Ingles", "id": "English"},
-                            {"name": "Historia", "id": "History"},
-                            {"name": "Geografia", "id": "Geography"},
-                            {"name": "Arte", "id": "Art"},
+                            {"name": "Matematicas", "id": "Matematicas"},
+                            {"name": "Literatura", "id": "Literatura"},
+                            {"name": "English", "id": "English"},
+                            {"name": "Deporte", "id": "Deporte"},
+                            {"name": "Geography", "id": "Geography"},
+                            {"name": "Art", "id": "Art"},
+                            {"name": "Biologia", "id": "Biologia"},
+                            {"name": "Orientacion", "id": "Orientacion"},
+                            {"name": "Participacion", "id": "Participacion"},
                             {"name": "Nota Promedio", "id": "Grade Average"},
                         ],
                         style_table={"overflowX": "auto"},
@@ -213,9 +211,8 @@ app.layout = dbc.Container(
         dbc.Row([dbc.Col(dcc.Graph(id="performance-over-time"), width=12)]),
     ],
     fluid=True,
-    style={"max-width": "1000px"},
+    style={"max-width": "1200px"},
 )
-
 
 # Callback to update the summary table
 @app.callback(
@@ -228,7 +225,6 @@ def update_summary_table(selected_student, selected_year):
     ]
     return filtered_summary_df.to_dict("records")
 
-
 # Callback to update the average grade card and student image
 @app.callback(
     [Output("average-grade", "children"), Output("student-image", "src")],
@@ -239,7 +235,6 @@ def update_average_grade_and_image(selected_student):
     average_grade = student_df["Grade Average"].mean()
     image_url = student_df["Image URL"].values[0]  # Get the image URL for the student
     return f"{average_grade:.2f}", image_url
-
 
 # Callback to update the exam table
 @app.callback(
@@ -269,7 +264,6 @@ def update_exam_table(selected_student, selected_year, selected_subject):
         }
     )
     return exam_df.to_dict("records")
-
 
 # Callback to update the performance over time line chart
 @app.callback(
@@ -301,7 +295,6 @@ def update_performance_chart(selected_student, selected_year, selected_subject):
     fig.update_layout(xaxis_title="Lapso", yaxis_title="Nota")
     return fig
 
-
 # Callback to update the subject performance bar chart
 @app.callback(
     Output("subject-performance-chart", "figure"),
@@ -322,12 +315,11 @@ def update_subject_performance_chart(selected_student, selected_year):
         chart_data,
         x="Subject",
         y="Grade",
-        title=f"Calificaciones de  {selected_student} en {selected_year}",
+        title=f"Calificaciones de {selected_student} en {selected_year}",
         color="Subject",
     )
     fig.update_layout(xaxis_title="Subject", yaxis_title="Grade")
     return fig
-
 
 # Run the app
 if __name__ == "__main__":

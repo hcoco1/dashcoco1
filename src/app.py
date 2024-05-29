7eb487c1fdb0e29739b8318536ef6d1f0515b7be
Ivan Arias <arias.ivan@gmail.com>
2024-05-28 21:31:05 -0400

    app updated

--- src/app.py
+++ src/app.py
@@ -1,22 +1,21 @@
 import pandas as pd
-from dash import Dash, dcc, html, Input, Output, dash_table
+from dash import Dash, Input, Output, dash_table, html, dcc
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import dash_auth
 
-
-
-
 # Load the new dataset
 df = pd.read_csv('https://raw.githubusercontent.com/hcoco1/Dashboard-Plothy-Dash/main/hcoco1/src/data/grades_over_time.csv')
 
+# Initialize the Dash app
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
-auth = dash_auth.BasicAuth(
-app,
-{'bugsbunny': 'topsecret'}
-)
 
+# Set the secret key for session management
+server.secret_key = 'supersecretkey'  # Replace with a strong secret key
+
+# Set up basic authentication
+auth = dash_auth.BasicAuth(app, {'bugsbunny': 'topsecret'})
 
 # Ensure grades are numeric
 for col in df.columns[3:]:
@@ -27,10 +26,10 @@ subjects = ["Matematicas", "Literatura", "English", "Deporte", "Geography", "Art
 for subject in subjects:
     df[f"{subject}"] = df[
         [f"{subject} Exam 1", f"{subject} Exam 2", f"{subject} Exam 3"]
-    ].mean(axis=1).round(2)
+    ].mean(axis=1).round(0)
 
 # Calculate the final grade average across all subjects
-df["Grade Average"] = df[[f"{subject}" for subject in subjects]].mean(axis=1).round(2)
+df["Grade Average"] = df[[f"{subject}" for subject in subjects]].mean(axis=1).round(0)
 
 # Prepare the summary table with only final grades
 summary_df = df[
@@ -40,7 +39,7 @@ summary_df = df[
 ]
 
 # Ensure all final grades have no more than two decimal places
-summary_df = summary_df.round(2)
+summary_df = summary_df.round(0)
 
 # Layout of the app
 app.layout = dbc.Container(
@@ -48,15 +47,8 @@ app.layout = dbc.Container(
         dbc.Row(
             [
                 dbc.Col(
-                    html.H1("Dashboard Interactivo Rendimiento Estudiantil"), className="mb-4"
-                )
-            ]
-        ),
-        dbc.Row(
-            [
-                dbc.Col(
                     [
-                        html.H4("Selecciona un Estudiante"),
+                        html.H5("Estudiante"),
                         dcc.Dropdown(
                             id="student-dropdown",
                             options=[
@@ -65,10 +57,30 @@ app.layout = dbc.Container(
                             ],
                             value=df["Name"].unique()[0],
                             clearable=False,
+                            style={"width": "200px"}
                         ),
                     ],
-                    width=4,
-                    style={"margin-bottom": "0px"},
+                    width=2,
+                    style={"margin-bottom": "10px", "margin-right":"10px"},
+                    className="mt-4"
+                ),
+                dbc.Col(
+                    [
+                        html.H5("Grado"),
+                        dcc.Dropdown(
+                            id="year-dropdown",
+                            options=[
+                                {"label": year, "value": year}
+                                for year in df["Year"].unique()
+                            ],
+                            value=df["Year"].unique()[0],
+                            clearable=False,
+                            style={"width": "200px"}
+                        ),
+                    ],
+                    width=2,
+                    style={"margin-bottom": "10px", "margin-right":"10px"},
+                    className="mt-4"
                 ),
                 dbc.Col(
                     [
@@ -84,8 +96,9 @@ app.layout = dbc.Container(
                                                         "width": "150px",
                                                         "height": "150px",
                                                         "display": "block",
-                                                        "margin": "auto",
+                                                        "margin-top": "10px",
                                                     },
+                                                    className="mt-4"
                                                 ),
                                                 html.H4(
                                                     id="average-grade",
@@ -111,8 +124,9 @@ app.layout = dbc.Container(
                             },
                         )
                     ],
-                    width=4,
+                    width=2,
                     style={"marginLeft": "auto", "border": "none"},
+                    className="mt-4"
                 ),
             ],
             className="align-items-start",
@@ -120,20 +134,31 @@ app.layout = dbc.Container(
         dbc.Row(
             [
                 dbc.Col(
-                    [
-                        html.H4("Selecciona un Año Escolar"),
-                        dcc.Dropdown(
-                            id="year-dropdown",
-                            options=[
-                                {"label": year, "value": year}
-                                for year in df["Year"].unique()
-                            ],
-                            value=df["Year"].unique()[0],
-                            clearable=False,
-                        ),
-                    ],
-                    width=4,
-                    style={"margin-top": "0px"},
+                    dash_table.DataTable(
+                        id="summary-table",
+                        columns=[
+                            {"name": "Nombre", "id": "Name"},
+                            {"name": "Grado", "id": "Year"},
+                            {"name": "Matematicas", "id": "Matematicas"},
+                            {"name": "Literatura", "id": "Literatura"},
+                            {"name": "English", "id": "English"},
+                            {"name": "Deporte", "id": "Deporte"},
+                            {"name": "Geografia", "id": "Geography"},
+                            {"name": "Arte", "id": "Art"},
+                            {"name": "Biologia", "id": "Biologia"},
+                            {"name": "Orientacion", "id": "Orientacion"},
+                            {"name": "Participacion", "id": "Participacion"},
+                            {"name": "Final Grado", "id": "Grade Average"},
+                        ],
+                        style_table={"overflowX": "auto"},
+                        style_header={
+                            "backgroundColor": "rgb(230, 230, 230)",
+                            "fontWeight": "bold",
+                        },
+                        style_cell={"textAlign": "center"},
+                    ),
+                    width=12,
+                    className="mb-4 mt-4",
                 )
             ]
         ),
@@ -146,39 +171,8 @@ app.layout = dbc.Container(
         dbc.Row(
             [
                 dbc.Col(
-                    dash_table.DataTable(
-                        id="summary-table",
-                        columns=[
-                            {"name": "Student Name", "id": "Name"},
-                            {"name": "Año Escolar", "id": "Year"},
-                            {"name": "Matematicas", "id": "Matematicas"},
-                            {"name": "Literatura", "id": "Literatura"},
-                            {"name": "English", "id": "English"},
-                            {"name": "Deporte", "id": "Deporte"},
-                            {"name": "Geography", "id": "Geography"},
-                            {"name": "Art", "id": "Art"},
-                            {"name": "Biologia", "id": "Biologia"},
-                            {"name": "Orientacion", "id": "Orientacion"},
-                            {"name": "Participacion", "id": "Participacion"},
-                            {"name": "Nota Promedio", "id": "Grade Average"},
-                        ],
-                        style_table={"overflowX": "auto"},
-                        style_header={
-                            "backgroundColor": "rgb(230, 230, 230)",
-                            "fontWeight": "bold",
-                        },
-                        style_cell={"textAlign": "left"},
-                    ),
-                    width=12,
-                    className="mb-4",
-                )
-            ]
-        ),
-        dbc.Row(
-            [
-                dbc.Col(
                     [
-                        html.H4("Selecciona una Materia"),
+                        html.H5("Materia"),
                         dcc.Dropdown(
                             id="subject-dropdown",
                             options=[
@@ -186,9 +180,10 @@ app.layout = dbc.Container(
                             ],
                             value=subjects[0],
                             clearable=False,
+                            style={"width": "200px"}
                         ),
                     ],
-                    width=4,
+                    width=2,
                 )
             ],
             className="mb-4",
@@ -199,8 +194,8 @@ app.layout = dbc.Container(
                     dash_table.DataTable(
                         id="exam-table",
                         columns=[
-                            {"name": "Student Name", "id": "Name"},
-                            {"name": "Año Escolar", "id": "Year"},
+                            {"name": "Nombre", "id": "Name"},
+                            {"name": "Grado", "id": "Year"},
                             {"name": "Lapso 1", "id": "Exam 1"},
                             {"name": "Lapso 2", "id": "Exam 2"},
                             {"name": "Lapso 3", "id": "Exam 3"},
@@ -210,7 +205,7 @@ app.layout = dbc.Container(
                             "backgroundColor": "rgb(230, 230, 230)",
                             "fontWeight": "bold",
                         },
-                        style_cell={"textAlign": "left"},
+                        style_cell={"textAlign": "center"},
                     ),
                     width=12,
                     className="mb-4",
@@ -241,9 +236,9 @@ def update_summary_table(selected_student, selected_year):
 )
 def update_average_grade_and_image(selected_student):
     student_df = summary_df[summary_df["Name"] == selected_student]
-    average_grade = student_df["Grade Average"].mean()
+    average_grade = student_df["Grade Average"].mean().round(0)
     image_url = student_df["Image URL"].values[0]  # Get the image URL for the student
-    return f"{average_grade:.2f}", image_url
+    return f" Promedio:{int(average_grade)}", image_url
 
 # Callback to update the exam table
 @app.callback(
@@ -300,8 +295,35 @@ def update_performance_chart(selected_student, selected_year, selected_subject):
         y="Nota",
         title=f"Notas por Lapso de {selected_student} en {selected_subject} ({selected_year})",
         markers=True,
+        line_shape='spline',  # Smooth the lines
     )
+    
+    # Customize line color and style
+    fig.update_traces(line=dict(color='black', width=4), marker=dict(symbol='x', size=10, color='red'))
+    
+    fig.add_shape(
+        type="line",
+        x0=0, x1=1, y0=10, y1=10,
+        line=dict(color="Red", width=1, dash="dash"),
+        xref="paper", yref="y"
+    )
+    
+    fig.add_shape(
+        type="line",
+        x0=0, x1=1, y0=15, y1=15,
+        line=dict(color="blue", width=1, dash="dash"),
+        xref="paper", yref="y"
+    )
+        
+    fig.add_shape(
+        type="line",
+        x0=0, x1=1, y0=18, y1=18,
+        line=dict(color="green", width=1, dash="dash"),
+        xref="paper", yref="y"
+    )
+    
     fig.update_layout(xaxis_title="Lapso", yaxis_title="Nota")
+    
     return fig
 
 # Callback to update the subject performance bar chart
